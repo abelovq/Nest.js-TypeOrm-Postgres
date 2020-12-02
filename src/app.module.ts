fda2e556421b5c4a8ab85b7dc6efac9d89707b27
Alex <flalex855@gmail.com>
2020-12-02 14:03:10 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { config } from './orm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [TypeOrmModule.forRoot(config), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
